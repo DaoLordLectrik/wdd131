6184fb82c45412da5976389d06e293337a564886
daolordlectrik <lectrikray@gmail.com>
2024-07-21 21:43:41 +0000

    commit

--- Project/scripts/scripts.js
+++ Project/scripts/scripts.js
@@ -16,3 +16,10 @@ document.getElementById("copyright").innerHTML = `&copy; ${currentYear} Fiifi C 
 const lastModifiedDate = document.lastModified;
   
 document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
+
+// Thank you message
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('contact-form').style.display = 'none';
+  document.getElementById('thank-you-message').style.display = 'block';
+});
